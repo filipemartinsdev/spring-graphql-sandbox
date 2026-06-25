dd9b83c8b65a25b96d2f45d3864f2b125dbbc483
filipemartinsdev <dev.filipemartins@gmail.com>
2026-06-25 19:26:57 -0300

    feat: implement query authorById

--- src/main/java/spring/graphql/sandbox/infra/web/AuthorController.java
+++ src/main/java/spring/graphql/sandbox/infra/web/AuthorController.java
@@ -1,12 +1,17 @@
 package spring.graphql.sandbox.infra.web;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import spring.graphql.sandbox.domain.Author;
+import spring.graphql.sandbox.domain.Book;
 import spring.graphql.sandbox.infra.persistence.AuthorRepository;
 
-// Instead of RestController on springbootMVC
+// Instead of RestController on Spring Boot MVC
 @Controller
 public class AuthorController {
   private final AuthorRepository authorRepository;
@@ -15,8 +20,13 @@ public class AuthorController {
     this.authorRepository = authorRepository;
   }
 
-  @QueryMapping()
+  @QueryMapping
   public Iterable<Author> authors(){
     return authorRepository.findAll();
+  }
+
+  @QueryMapping
+  public Optional<Author> authorById(@Argument Long id){
+    return authorRepository.findById(id);
   }
 }
