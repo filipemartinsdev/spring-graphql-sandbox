8f733d01240e2b76eba75cfee260106cdfb5efd8
filipemartinsdev <dev.filipemartins@gmail.com>
2026-06-26 15:18:41 -0300

    feat: create graphql consumer for test

--- src/main/java/spring/graphql/sandbox/SandboxApplication.java
+++ src/main/java/spring/graphql/sandbox/SandboxApplication.java
@@ -1,9 +1,13 @@
 package spring.graphql.sandbox;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.graphql.client.HttpGraphQlClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import spring.graphql.sandbox.domain.Author;
 import spring.graphql.sandbox.domain.Book;
@@ -17,47 +21,26 @@ public class SandboxApplication {
 		SpringApplication.run(SandboxApplication.class, args);
 	}
 
-	private final AuthorRepository authorRepository;
-	private final BookRepository bookRepository;
-
-	public SandboxApplication(AuthorRepository authorRepository, BookRepository bookRepository){
-		this.authorRepository = authorRepository;
-		this.bookRepository = bookRepository;
-	}
-
 	@Bean
 	public CommandLineRunner runner(){
 		return args -> {
-			if (bookRepository.count() == 0) {
-				populateDB();
-			}
+			testQuery();
 		};
 	}
 
-	private void populateDB(){
-		var douglasAdams = new Author();
-		douglasAdams.setName("Douglas Adams");
+	private void testQuery(){
+		WebClient webClient = WebClient.builder()
+			.baseUrl("http://localhost:8080/graphql")
+			.build();
+
+		HttpGraphQlClient client = HttpGraphQlClient.builder(webClient)
+			.build();
 		
-		var book1 = new Book();
-		book1.setAuthor(douglasAdams);
-		book1.setTitle("O guia definitivo do mochileiro das galaxias");
-		douglasAdams.getBooks().add(book1);
+		List<Book> books = client.documentName("books")
+			.retrieve("books")
+			.toEntityList(Book.class)
+			.block();
 
-		authorRepository.save(douglasAdams);	
-
-		var george = new Author();
-		george.setName("George Orwell");
-		
-		var book2 = new Book();
-		book2.setAuthor(george);
-		book2.setTitle("1984");
-		george.getBooks().add(book2);
-
-		var book3 = new Book();
-		book3.setTitle("A revolução dos bichos");
-		book3.setAuthor(george);
-		george.getBooks().add(book3);
-
-		authorRepository.save(george);
+		System.out.println("Books: " + books);
 	}
 }
