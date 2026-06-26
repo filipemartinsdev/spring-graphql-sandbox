532c5604f0523452d2f9075edfc2fc430381068f
filipemartinsdev <dev.filipemartins@gmail.com>
2026-06-25 21:34:22 -0300

    feat: implement pagination with Relay/Cursor pattern

--- src/main/java/spring/graphql/sandbox/infra/persistence/AuthorRepository.java
+++ src/main/java/spring/graphql/sandbox/infra/persistence/AuthorRepository.java
@@ -1,9 +1,12 @@
 package spring.graphql.sandbox.infra.persistence;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.ScrollPosition;
+import org.springframework.data.domain.Window;
+import org.springframework.data.repository.CrudRepository;
 
 import spring.graphql.sandbox.domain.Author;
 
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-  
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+    Window<Author> findByOrderByIdAsc(ScrollPosition position, Limit limit);
 }
--- src/main/java/spring/graphql/sandbox/infra/web/AuthorController.java
+++ src/main/java/spring/graphql/sandbox/infra/web/AuthorController.java
@@ -1,14 +1,22 @@
 package spring.graphql.sandbox.infra.web;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Limit;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.ScrollPosition;
+import org.springframework.data.domain.Window;
+import org.springframework.data.jpa.support.PageableUtils;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.query.ScrollSubrange;
 import org.springframework.stereotype.Controller;
 
+import io.github.responsekit.core.PagedResponse;
 import spring.graphql.sandbox.domain.Author;
-import spring.graphql.sandbox.domain.Book;
 import spring.graphql.sandbox.infra.persistence.AuthorRepository;
 
 // Instead of RestController on Spring Boot MVC
@@ -28,5 +36,14 @@ public class AuthorController {
   @QueryMapping
   public Optional<Author> authorById(@Argument Long id){
     return authorRepository.findById(id);
+  }
+
+  @QueryMapping
+  public Window<Author> pagedAuthors(ScrollSubrange subrange){
+    ScrollPosition position = subrange.position().orElse(ScrollPosition.keyset());
+
+    Limit limit = Limit.of(subrange.count().orElse(10));
+
+    return authorRepository.findByOrderByIdAsc(position, limit);
   }
 }
